742bae5b2409c8e02d18dd3613520c020b914729
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-26 05:48:40 +0100

    cats api db

--- docker_nana_course/application/cats-app/server.mjs
+++ docker_nana_course/application/cats-app/server.mjs
@@ -1,9 +1,11 @@
 import express from "express"
 import {v4 as uuidv4} from 'uuid';
+import { MongoClient } from "mongodb";
 
 const app = express()
 const port = 3000
 
+/*
 let cats_list = [
 	"Abby",
 	"Angel",
@@ -112,7 +114,33 @@ let list_with_id = []
 for ( let index in cats_list ){
 	list_with_id.push({id: uuidv4(), name: cats_list[index]})
 }
+*/
 
+
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'cats-app';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
 
 app.get('/', (req, res)=>{
     res.json(list_with_id)
